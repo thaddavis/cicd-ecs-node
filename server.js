2996651dd8e2e1a990a93bcbe9cebbb8a93911a3
Tad <a@as-MacBook-Pro.local>
2021-06-16 13:37:53 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color:green;">Sample Node App - version-1!!</h1> \n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
